b2f4ffc2ba2bec509d0bb7ba865bc3e6f5ea1fda
Baluj96 <110256191+Baluj96@users.noreply.github.com>
2024-03-11 17:57:39 +0100

    Conecta3
    Comprobación de cuantas piezas están conectadas

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -9,6 +9,8 @@ public class Board : MonoBehaviour
     public GameObject tileObject;
     public float camSizeOffset, camVerticalOffset;
     public GameObject[] avalaiblePieces;
+    List<GameObject> filaPieces = new List<GameObject>();
+    List<GameObject> colmnPieces = new List<GameObject>();
 
     Tile[,] Tiles;
     Piece[,] Pieces;
@@ -31,7 +33,7 @@ public class Board : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity, gameObject.transform);
+                var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity, gameObject.transform.GetChild(0).transform);
                 Tiles[x, y] = o.GetComponent<Tile>();
                 Tiles[x, y]?.Setup(x, y, this);
             }
@@ -56,7 +58,7 @@ public class Board : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 int r = Random.Range(0, avalaiblePieces.Length);
-                var o = Instantiate(avalaiblePieces[r], new Vector3(x, y, -5), Quaternion.identity, gameObject.transform);
+                var o = Instantiate(avalaiblePieces[r], new Vector3(x, y, -5), Quaternion.identity, gameObject.transform.GetChild(1).transform);
                 o.GetComponent<Piece>().Setup(x,y, this);
                 Pieces[x, y] = o.GetComponent<Piece>();
                 Pieces[x, y]?.Setup(x, y, this);
@@ -91,7 +93,130 @@ public class Board : MonoBehaviour
         EndPiece.Move(startTile.x, startTile.y);
 
         Pieces[startTile.x, startTile.y] = EndPiece;
+        CheckMatch3(EndPiece);
+
         Pieces[endTile.x, endTile.y] = StartPiece;
+        CheckMatch3(StartPiece); 
+    }
+
+    void CheckMatch3(Piece piece_)
+    {
+        //Limpia las listas
+        colmnPieces.Clear();
+        filaPieces.Clear();
+
+        //Añade la pieza a ambas listas
+        colmnPieces.Add(piece_.gameObject);
+        filaPieces.Add(piece_.gameObject);
+
+        if (piece_.y + 1 <= 9)
+        {
+            //Si la pieza de arriba está dentro de los márgenes comprueba en dicha dirección
+            CheckMatch3Up(piece_);
+        }
+        if (piece_.y - 1 >= 0)
+        {
+            //Si la pieza de abajo está dentro de los márgenes comprueba en dicha dirección
+            CheckMatch3Down(piece_);
+        }
+        if (piece_.x + 1 <= 5)
+        {
+            //Si la pieza de la derecha está dentro de los márgenes comprueba en dicha dirección
+            CheckMatch3Right(piece_);
+        }
+        if (piece_.x - 1 >= 0)
+        {
+            //Si la pieza de la izquierda está dentro de los márgenes comprueba en dicha dirección
+            CheckMatch3Left(piece_);
+        }
+
+        if (colmnPieces.Count >= 3)
+        {
+            //Si hay una columna de al menos 3 piezas de largo, las destruye
+            for (int i = 0; i < colmnPieces.Count; i++)
+            {
+                Destroy(colmnPieces[i]);
+            }
+        }
+
+        if (filaPieces.Count >= 3)
+        {
+            //Si hay una fila de al menos 3 piezas de largo, las destruye
+            for (int i = 0; i < colmnPieces.Count; i++)
+            {
+                Destroy(filaPieces[i]);
+            }
+        }
+    }
+
+
+    void CheckMatch3Up(Piece piece_)
+    {
+        Debug.Log(piece_.y + 1);
+        Piece pieceUp = Pieces[piece_.x, piece_.y + 1];
+
+        if (piece_.GetComponent<Piece>().pieceType == pieceUp.GetComponent<Piece>().pieceType)
+        {
+            //Si son iguales, añade la pieza a la lista de columna
+            colmnPieces.Add(pieceUp.gameObject);
+
+            if (pieceUp.y + 1 <= 9)
+            {
+                //Si la pieza de arriba está dentro de los márgenes comprueba en dicha dirección
+                CheckMatch3Up(pieceUp);
+            }
+        }
+    }
+
+    void CheckMatch3Down(Piece piece_)
+    {
+        Piece pieceDown = Pieces[piece_.x, piece_.y - 1];
+
+        if (piece_.GetComponent<Piece>().pieceType == pieceDown.GetComponent<Piece>().pieceType)
+        {
+            //Si son iguales, añade la pieza a la lista de columna
+            colmnPieces.Add(pieceDown.gameObject);
+
+            if (pieceDown.y - 1 >= 0)
+            {
+                //Si la pieza de abajo está dentro de los márgenes comprueba en dicha dirección
+                CheckMatch3Down(pieceDown);
+            }            
+        }
+    }
+
+    void CheckMatch3Right(Piece piece_)
+    {
+        Piece pieceRight = Pieces[piece_.x + 1, piece_.y];
+
+        if (piece_.GetComponent<Piece>().pieceType == pieceRight.GetComponent<Piece>().pieceType)
+        {
+            //Si son iguales, añade la pieza a la lista de fila
+            filaPieces.Add(pieceRight.gameObject);
+
+            if (pieceRight.x + 1 <= 5)
+            {
+                //Si la pieza de la derecha está dentro de los márgenes comprueba en dicha dirección
+                CheckMatch3Right(pieceRight);
+            }
+        }
+    }
+
+    void CheckMatch3Left(Piece piece_)
+    {
+        Piece pieceLeft = Pieces[piece_.x - 1, piece_.y];
+
+        if (piece_.GetComponent<Piece>().pieceType == pieceLeft.GetComponent<Piece>().pieceType)
+        {
+            //Si son iguales, añade la pieza a la lista de fila
+            filaPieces.Add(pieceLeft.gameObject);
+
+            if (pieceLeft.x - 1 >= 0)
+            {
+                //Si la pieza de la izquierda está dentro de los márgenes comprueba en dicha dirección
+                CheckMatch3Left(pieceLeft);
+            }
+        }
     }
 
     public bool IsCloseUp(Tile start, Tile end)
